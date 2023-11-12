d1e990dcf4e288588322062bba4f834097ba3c1a
Ruminate <inramiland@gmail.com>
2023-11-12 16:55:21 -0500

    function for updating current weather

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,24 @@ const APIKEY ="55f55ad7babd3c5f4fdd516e4df53a7a";
 const searchBtn = document.querySelector(".search-btn");
 const cityInput = document.querySelector(".city-input");
 
+const updateCurrentWeather = (data) => {
+    const location = data.name;
+    const tempCelsius = data.main.temp;
+    const windSpeed = data.wind.speed;
+    const humidity = data.main.humidity;
+
+    const tempFahrenheit = celsiusToFahrenheit(tempCelsius);
+
+    document.querySelector(".details h2").textContent = location;
+    document.querySelector(".details h3:nth-child(2)").textContent = `Temp: ${tempFahrenheit.toFixed(2)}°F`;
+    document.querySelector(".details h3:nth-child(3)").textContent = `Wind Speed: ${windSpeed} m/s`;
+    document.querySelector(".details h3:nth-child(4)").textContent = `Humidity: ${humidity}%`;
+};
+
+const updateForecast = (forecastData) => {
+    
+}
+
 const getLocation = () => {
     const city =cityInput.value.trim();
     if(!city) return;
