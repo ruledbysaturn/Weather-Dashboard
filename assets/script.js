8cb11ab4e22f1a38634b374ae67be11487af8838
Ruminate <inramiland@gmail.com>
2023-11-12 16:21:28 -0500

    fixed minor issues with javascript

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,22 @@
-var APIKEY ="55f55ad7babd3c5f4fdd516e4df53a7a";
-
+const APIKEY ="55f55ad7babd3c5f4fdd516e4df53a7a";
 const searchBtn = document.querySelector(".search-btn");
 const cityInput = document.querySelector(".city-input");
 
 const getLocation = () => {
     const city =cityInput.value.trim();
     if(!city) return;
-    const geocoding_url = "api.openweathermap.org/data/2.5/weather?q={city}&appid={APIKEY}";
+    const geocoding_url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid={APIKEY}`;
 
-    fetch(geocoding_url).then(res => res.json()).then(data => {
-        console.log(data)
-    }).catch()
-        alert("Error occurred while fetching coordinates");
+    fetch(geocoding_url).then((res) => res.json()).then((data) => {
+        console.log(data);
+    })
+    
+    .catch((error) => {
 
-    };
+        console.error("Error occurred while fetching coordinates", error);
+
+    })
+};
 
 
 searchBtn.addEventListener("click", getLocation);
